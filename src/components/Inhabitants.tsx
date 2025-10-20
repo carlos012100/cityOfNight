0745b3c4e2fa94b4b576f232e35ea5ae4e9465aa
carlos012100 <carlos012100@gmail.com>
2025-10-20 13:45:13 +0200

    add components new

--- src/components/Inhabitants.tsx
+++ src/components/Inhabitants.tsx
@@ -1,15 +1,32 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import './Styles/Inhabitants.css'
 import './Styles/DefensesStyles.css'
 
 export default function Inhabitants({populationData}) {
+  const populationNumbers = populationData.roles.map()
+
+  console.log(populationNumbers)
+
+  const [details, setDetails] = useState(false);
+  const showDetails = () =>{
+      setDetails(prev => !prev)
+
+
+  }
   return (
     <div className='frame'>
-      <ul>
-        {populationData.roles.map((population, index) => (
-          <li key={index} className='classList'>{population.role}</li>
+      <div>
+        <h1 className='h1'>Inhabitants</h1>
+        <p className='p'><strong>Total Population: {}</strong></p>
+          {populationData.roles.map((population, index) => (
+          // <li  key={index}>
+          //    {population.role}
+          // </li>
+          <p key={index}><button className='button'>{population.role}</button></p>
         ))}
-      </ul>
+
+      </div>
+
     </div>
   )
 }
